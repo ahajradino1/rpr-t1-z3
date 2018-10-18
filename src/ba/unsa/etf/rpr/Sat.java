3393f34efcdd5a2fc0d871c63be5a8ecba6af2cf
ahajradino1 <44201261+ahajradino1@users.noreply.github.com>
2018-10-18 09:49:26 +0200

    second commit

---
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,56 @@
+package ba.unsa.etf.rpr;
+
+public class Sat {
+    private int sati;
+    private int minute;
+    private int sekunde;
+    Sat( int sati, int minute, int sekunde) {
+        postavi( sati, minute, sekunde );
+    }
+    private void postavi( int sati, int minute, int sekunde ) {
+        this.sati = sati;
+        this.minute = minute;
+        this.sekunde = sekunde;
+    }
+    private void sljedeci() {
+        sekunde++;
+        if( sekunde == 60 ) { sekunde = 0; minute++; }
+        if( minute == 60 ) { minute = 0; sati++; }
+        if( sati == 24 ) sati = 0;
+
+    }
+    private void prethodni() {
+        sekunde--;
+        if( sekunde == -1 ) { sekunde = 59; minute--; }
+        if( minute == -1 ) { minute = 59; sati--; }
+        if( sati == -1 ) sati = 23;
+    }
+    private void pomjeriZa( int pomak ) {
+        if( pomak > 0)
+            for( int i = 0; i < pomak; i++ ) sljedeci();
+        else
+            for(int i = 0; i < -pomak; i++) prethodni();
+    }
+    public int getSati() {
+        return sati;
+    }
+    public int getMinute() {
+        return minute;
+    }
+    public int getSekunde() {
+        return sekunde;
+    }
+    private void ispisi()  {
+        System.out.println(sati + ":" + minute + ":" + sekunde);
+    }
+    public static void main(String[] args) {
+        Sat s = new Sat( 15, 30, 45 );
+        s.ispisi();
+        s.sljedeci();
+        s.ispisi();
+        s.pomjeriZa(-48);
+        s.ispisi();
+        s.postavi(0,0,0);
+        s.ispisi();
+    }
+}
